2752bb31e87f062510b6c33e03ff2af4767adcb8
Guillermo Otegui <guilleotegui03@gmail.com>
2024-07-15 21:04:07 -0300

    wip: ai response interface

--- components/response.tsx
+++ components/response.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+"use client";
+
+import { useStore } from "@/utils/store";
 
 export const Response = () => {
-  return <div className="w-full rounded-lg bg-slate-100 h-full">Response</div>;
+  const { response, score } = useStore();
+
+  return (
+    <div className="w-full rounded-lg bg-slate-100 h-full">
+      <p>{response}</p>
+      <p>Resultado final: {score}</p>
+    </div>
+  );
 };
