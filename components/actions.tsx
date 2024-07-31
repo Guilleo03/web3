e8855461c3e8e4b64e576cdfc4cbe31e0b016d95
Guillermo Otegui <guilleotegui03@gmail.com>
2024-07-31 00:36:06 -0300

    fix: delete photo button

--- components/actions.tsx
+++ components/actions.tsx
@@ -58,12 +58,12 @@ const Actions = () => {
   return (
     <>
       <div>{error && <p className="text-destructive">{error}</p>}</div>
-      <div className="flex justify-end gap-6 mt-6">
+      <div className="flex justify-center gap-6 mt-6">
         <div className="flex gap-6">
           {image && (
             <Button
               onClick={handleDelete}
-              className="px-8 hover:bg-destructive flex items-center gap-x-1"
+              className="px-4 md:px-8 hover:bg-destructive flex items-center gap-x-1"
               variant={"destructive"}
             >
               <Trash className="h-4 w-4" />
@@ -73,7 +73,7 @@ const Actions = () => {
           <form action={requestInfo}>
             <Button
               type="submit"
-              className="px-8 hover:bg-primary hover:opacity-80 transition-all flex items-center gap-x-1 dark:bg-primary bg-gradient-to-r from-primary to-[#29c233] dark:to-primary"
+              className="px-4 md:px-8 hover:bg-primary hover:opacity-80 transition-all flex items-center gap-x-1 dark:bg-primary bg-gradient-to-r from-primary to-[#29c233] dark:to-primary"
               disabled={image ? false : true}
             >
               <Sparkles className="h-4 w-4" />
