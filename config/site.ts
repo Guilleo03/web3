2b2f13c4fa13239ccabb7c1a1e62c2d0f5fe4546
Guillermo Otegui <guilleotegui03@gmail.com>
2024-07-31 09:35:19 -0300

    chore: update metadata

--- config/site.ts
+++ config/site.ts
@@ -2,13 +2,8 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "FoodAI",
-  description: "Escanea un producto envasado y te dire si es saludable o no",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-  ],
+  description:
+    "Sube una imagen o toma una foto de los ingredientes o tabla nutricional del producto envasado y te dire si es saludable.",
   links: {
     linkedin: "https://www.linkedin.com/in/guillermo-otegui/",
   },
